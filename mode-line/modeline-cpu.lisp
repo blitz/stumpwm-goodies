64cd9e4a3f0fb13d3b7ff2526849a1920c7e4b6f
Julian Stecklina <js@alien8.de>
2009-11-09 22:04:22 +0100

    Avoid calling READ (with *READ-EVAL* set to true... D'oh!).
    There are two major problems with the old code:
     - if the kernel decides to pass random strings, we leak memory by
       creating lots of symbols.
     - if the input contains #. we evaluate random code.

--- mode-line/modeline-cpu.lisp
+++ mode-line/modeline-cpu.lisp
@@ -8,32 +8,35 @@
        First value is percent of CPU in use.
        Second value is percent of CPU in use by system processes.
        Third value is percent of time since last call spent waiting for IO (or 0 if not available)."
-  (let ((cpu-result 0)
-        (sys-result 0)
-        (io-result nil))
-    (with-open-file (in "/proc/stat" :direction :input)
-      (if (eq 'cpu (read in))
-          (let* ((norm-user (read in))
-                 (nice-user (read in))
-                 (user (+ norm-user nice-user))
-                 (sys (read in))
-                 (idle (read in))
-                 (iowait (or (ignore-errors (read in)) 0))
-                 (step-denom (- (+ user sys idle iowait)
-                                (+ *prev-user-cpu* *prev-sys-cpu* *prev-idle-cpu* *prev-iowait*))))
-            (setf cpu-result (/ (- (+ user sys)
-                                   (+ *prev-user-cpu* *prev-sys-cpu*))
-                                step-denom)
-                  sys-result (/ (- sys *prev-sys-cpu*)
-                                step-denom)
-                  io-result (/ (- iowait *prev-iowait*)
-                               step-denom)
-                  *prev-user-cpu* user
-                  *prev-sys-cpu* sys
-                  *prev-idle-cpu* idle
-                  *prev-iowait* iowait))
-          (warn "Unexpected header")))
-    (values cpu-result sys-result io-result)))
+  (with-open-file (in "/proc/stat" :direction :input)
+    (loop 
+       for line = (read-line in nil nil)
+       while line
+       do (let ((match 
+                 (nth-value 1 (cl-ppcre:scan-to-strings "^cpu +(\\d+) +(\\d+) +(\\d+) +(\\d+)( +(\\d+)|)" line))))
+            (when match
+              (let* ((norm-user (parse-integer (aref match 0)))
+                     (nice-user (parse-integer (aref match 1)))
+                     (user (+ norm-user nice-user))
+                     (sys (parse-integer (aref match 2)))
+                     (idle (parse-integer (aref match 3)))
+                     (iowait (parse-integer (or (aref match 4) "0")))
+                     (step-denom (- (+ user sys idle iowait)
+                                    (+ *prev-user-cpu* *prev-sys-cpu* *prev-idle-cpu* *prev-iowait*)))
+                     (cpu-result (/ (- (+ user sys)
+                                       (+ *prev-user-cpu* *prev-sys-cpu*))
+                                    step-denom))
+                     (sys-result (/ (- sys *prev-sys-cpu*)
+                                    step-denom))
+                     (io-result (/ (- iowait *prev-iowait*)
+                                   step-denom)))
+                (setf *prev-user-cpu* user
+                      *prev-sys-cpu* sys
+                      *prev-idle-cpu* idle
+                      *prev-iowait* iowait)
+                (return-from current-cpu-usage
+                  (values cpu-result sys-result io-result))))))
+    (values 0 0 0)))
 
 (defun format-current-cpu-usage (stream)
   "Formats a string representing the current processor usage to STREAM.
